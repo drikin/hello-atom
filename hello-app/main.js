669853ba7c6c3e60c2fa5f5de67f904620989f99
Koh Aoki <Kohichi.Aoki@am.sony.com>
2014-12-23 17:05:24 -0800

    playground

--- hello-app/main.js
+++ hello-app/main.js
@@ -19,6 +19,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.inspectElement(0, 0);
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
